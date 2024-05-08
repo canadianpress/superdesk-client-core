4e540126e5d1f4a489fc50d0501611c2d1f0a59b
cmuldur <clm@cp.org>
2024-05-08 11:50:06 -0400

    Fix optional properties in ITagUi interface

--- scripts/extensions/auto-tagger/src/types.ts
+++ scripts/extensions/auto-tagger/src/types.ts
@@ -2,8 +2,8 @@ export interface ITagUi {
     name: string;
     description?: string;
     qcode: string;
-    score: string;
-    creator: string;
+    score?: string;
+    creator?: string;
     source?: string;
     original_source?: string;
     aliases?: Array<string>;
