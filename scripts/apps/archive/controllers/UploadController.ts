76f8e7e6ce89a2af37bde8408b88b0ba76031eb7
Taylor Li <t4ylor.li@gmail.com>
2025-08-07 11:03:46 -0400

    update exiftool package

--- scripts/apps/archive/controllers/UploadController.ts
+++ scripts/apps/archive/controllers/UploadController.ts
@@ -8,7 +8,7 @@ import {appConfig} from 'appConfig';
 import {fileUploadErrorModal} from './file-upload-error-modal';
 import {showModal} from '@sourcefabric/common';
 import {sdApi} from 'api';
-import {parseMetadata} from '@taylorli2000/exiftool/cjs';
+import {parseMetadata} from '@uswriting/exiftool/cjs';
 import {IContentProfileType} from 'apps/workspace/content/controllers/ContentProfilesController';
 import {EXIFTOOL_ARGS, IPTC_XMP_TAGS, XMP_IPTC_TAGS} from 'apps/archive/constants';
 import {getObjectEntries} from 'utils/object';
